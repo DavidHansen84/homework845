522bb7ab6f498924e69b2333bf2948ae9dcbaf87
David Hansen <dracozlair@gmail.com>
2023-11-01 11:49:59 +0100

    Added content

--- routes/index.js
+++ routes/index.js
@@ -51,4 +51,41 @@ router.post("/favNumber", async (req, res) => {
   });
 });
 
+router.get("/content", async (req, res, next) => {
+  let my_file = await s3.getObject({
+    Bucket: process.env.CYCLIC_BUCKET_NAME,
+    Key: "content.json",
+  }).promise()
+  const result = JSON.parse(my_file.Body)?.favouriteNumber;
+  if(result == null) {
+    res.json({
+      status: "fail",
+    });
+  } else {
+  res.json({
+    status: "success",
+    result: result,
+  });
+}
+});
+
+router.post("/content", async (req, res, next) => {
+  const {content} = req.body;
+  if(content == null ) {
+    res.status(400).send("Content not provided");
+    return;
+  }
+  const contentObj = { content: content} 
+  
+  await s3.putObject({
+    Body: JSON.stringify(contentObj, null, 2),
+    Bucket: process.env.CYCLIC_BUCKET_NAME,
+    Key: "content.json"
+  }).promise()
+  res.json({
+    status: "success",
+    content: content,
+  });
+});
+
 module.exports = router;
